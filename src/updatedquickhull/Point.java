13c9f97505bab031e044f9aa76bdfd17f372fbe6
Ypigma <60528495+Ypigma@users.noreply.github.com>
2022-10-12 10:12:43 -0400

    Add files via upload

---
+++ src/updatedquickhull/Point.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Point
+{
+    public double x;
+    public double y;
+    public  Point( double x ,double y)
+    {
+        this.x =x;
+        this.y=y;
+    }
+
+
+    public String toString(){
+        return "{ " + x+ ", " + y + " }";
+    }
+}
